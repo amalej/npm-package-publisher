ce3a1c2db3efec0986535fc3a96b217544f012f4
amalej <alejandroalejandre15@gmail.com>
2023-10-10 04:52:32 +0800

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ npm install
 package='typescript'
 if [ `npm list | grep -c $package` -eq 0 ]; 
     then
+        echo "TypeScript not detected"
+    else
         echo "TypeScript detected"
         npx tsc
-    else
-        echo "TypeScript not detected"
 fi
 
 
